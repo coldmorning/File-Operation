177b87e6ec8665a6d14b397327d1f446228b042f
coldmorning <yulianghuang360@gmail.com>
2018-11-16 22:49:58 +0800

    add (argv: a, b) to choose function

--- SearchFiles_Recursion.py
+++ SearchFiles_Recursion.py
@@ -28,13 +28,14 @@ def originOsWalk(path):
             print("file:"+join(root, f))
         for d in dirs:
             print("directory:"+join(root, d))
-            
+
 #Practice  a simple version: os.walk       
 def ScanDir(path):
     files = os.listdir(path)
     for f in files:
         abspath=join(path, f)
         SearchFile(abspath)
+
 def SearchFile(abspath):
         
     if isfile(abspath):
@@ -43,14 +44,32 @@ def SearchFile(abspath):
     elif isdir(abspath):
         print("directory:", abspath)
         ScanDir(abspath)
+
 #Run above of two functions
-param = sys.arg[1]
-
-
+def Run(num, OriPath):
+    #https://docs.python.org/2/faq/design.html#why-isn-t-there-a-switch-or-case-statement-in-python
+    functions = {
+        'a': originOsWalk,
+        'b': ScanDir
+    }
+    func = functions.get(num,'invalid number')
+    func(OriPath)
+'''
+ python SearchFiles_Recursion.py a 
+                                    : originOsWalk
+ python SearchFiles_Recursion.py b
+                                    :   ScanDir
+'''
 OriPath = r"C:\Users\yug\Downloads"
+if len(sys.argv)>1:
+    num = sys.argv[1]
+    Run(num, OriPath)
 
 
-ScanDir(OriPath)# or originOsWalk(OriPath)
+
+
+
+ 
  
  
        
