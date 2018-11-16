a8fc113c8e8ff1baf2346f4dc147f27a6a28c47b
coldmorning <yulianghuang360@gmail.com>
2018-11-16 21:54:12 +0800

    more detail about practice

--- SearchFiles_Recursion.py
+++ SearchFiles_Recursion.py
@@ -1,43 +1,56 @@
 # coding=UTF-8
 '''
-Pratice a simple version: os.walk
---------------------
+We can search directory  in the directory tree.
+--------------------------------------
 file
 file
 folder
-----file
-----file
-----file
+｜----file
+｜----file
+｜----file
 file 
 file 
 folder
------file
------file
------foder
-     ----file
-     ----file
-
+｜----file
+｜----file
+｜----foder
+     ｜----file
+     ｜----file
 '''
-import os 
 
+import os, sys
+from os.path import join, isfile, isdir
+
+#Official os.walk()
+def originOsWalk(path):
+    for root, dirs, files in os.walk(path):
+        for f in files:
+            print("file:"+join(root, f))
+        for d in dirs:
+            print("directory:"+join(root, d))
+            
+#Practice  a simple version: os.walk       
 def ScanDir(path):
     files = os.listdir(path)
     for f in files:
-        abspath=os.path.join(path,f)
+        abspath=join(path, f)
         SearchFile(abspath)
-
-
 def SearchFile(abspath):
         
-    if os.path.isfile(abspath):
-        print("file:",abspath)
+    if isfile(abspath):
+        print("file:", abspath)
 
-    elif os.path.isdir(abspath):
-        print("folder:",abspath)
+    elif isdir(abspath):
+        print("directory:", abspath)
         ScanDir(abspath)
+#Run above of two functions
+param = sys.arg[1]
+
 
 OriPath = r"C:\Users\yug\Downloads"
-ScanDir(OriPath)
+
+
+ScanDir(OriPath)# or originOsWalk(OriPath)
  
  
        
